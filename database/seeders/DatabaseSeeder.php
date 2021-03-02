63f296d59caa087fd69ff1eacc0f76c67b3eea07
Fadak <fadak.alshiblawi@gmail.com>
2021-03-02 22:28:49 +0300

    add factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(150)->create();
+        \App\Models\Category::factory(15)->create();
+        \App\Models\Post::factory(500)->create();
+        \App\Models\Comment::factory(1500)->create();
+
     }
 }
